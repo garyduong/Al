7ed6ed218f4e1329aaa6eb8e495df53db9a234e2
Gary Duong <gx.duong@gmail.com>
2020-12-06 21:48:59 -0800

    Renamed to run for Lambda

--- run.js
+++ run.js
@@ -1,9 +1,12 @@
 const Discord = require('discord.js');
 const client = new Discord.client();
 
+
+// taken liberally from https://stackoverflow.com/questions/47548081/send-scheduled-message
+// send notification of weekly games night
 client.login(process.env.CLIENT_ID).then(() => {
     var guild = client.guilds.get(process.env.GUILD_ID);
-    if (guild && guild.channels.get(process.env.CHANNEL_ID)){
+    if (guild && guild.channels.get(process.env.CHANNEL_ID)) {
         guild.channels.get(process.env.CHANNEL_ID).send(
             '@everyone There will be a games night Friday at 8. Please let us know if you can join!'
         ).then(() => client.destroy());
