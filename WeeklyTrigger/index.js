b66144ccfa1f713cf376b8f68e803ddee931fbf3
Gary Duong <gx.duong@gmail.com>
2020-12-08 16:10:36 -0800

    Azure testing

---
+++ WeeklyTrigger/index.js
@@ -0,0 +1,26 @@
+const Discord = require('discord.js');
+const client = new Discord.client();
+
+module.exports = async function (context, myTimer) {
+    var timeStamp = new Date().toISOString();
+    
+    if (myTimer.isPastDue)
+    {
+        context.log('JavaScript is running late!');
+    }
+    send_announcement();
+    context.log('JavaScript timer trigger function ran!', timeStamp);   
+};
+
+function send_announcement() {
+    // taken liberally from https://stackoverflow.com/questions/47548081/send-scheduled-message
+    // send notification of weekly games night
+    return client.login(process.env.CLIENT_ID).then(() => {
+        var guild = client.guilds.get(process.env.GUILD_ID);
+        if (guild && guild.channels.get(process.env.CHANNEL_ID)) {
+            guild.channels.get(process.env.CHANNEL_ID).send(
+                '@everyone There will be a games night Friday at 8. Please let us know if you can join!'
+            ).then(() => client.destroy());
+        }
+    });
+}
