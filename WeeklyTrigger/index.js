50343a941073b6e2bac54c099af9ee1c52b1611a
Gary Duong <gx.duong@gmail.com>
2020-12-08 22:50:47 -0800

    Made send module async

--- WeeklyTrigger/index.js
+++ WeeklyTrigger/index.js
@@ -1,19 +1,15 @@
 module.exports = async function (context, myTimer) {
-    var timeStamp = new Date().toISOString();
-    
-    if (myTimer.isPastDue)
-    {
-        context.log('JavaScript is running late!');
-    }
-    context.log('JavaScript timer trigger function ran!', timeStamp);   
+    send_announcement().then(() => {
+        context.log('Weekly announcement was posted!')
+    }).catch(e => context.log(e));
 };
 
 // taken liberally from https://stackoverflow.com/questions/47548081/send-scheduled-message
 // send notification of weekly games night
-function send_announcement() {
+async function send_announcement() {
     const Discord = require('discord.js');
     const client = new Discord.client();
-    return client.login(process.env.CLIENT_ID).then(() => {
+    return await client.login(process.env.CLIENT_ID).then(() => {
         var guild = client.guilds.get(process.env.GUILD_ID);
         if (guild && guild.channels.get(process.env.CHANNEL_ID)) {
             guild.channels.get(process.env.CHANNEL_ID).send(
