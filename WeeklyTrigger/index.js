cf16d4b74b584fe214deddbac66056165194a302
Gary Duong <gx.duong@gmail.com>
2020-12-10 13:06:09 -0800

    Remove async code for now; set exec by timer

--- WeeklyTrigger/index.js
+++ WeeklyTrigger/index.js
@@ -1,15 +1,17 @@
 module.exports = async function (context, myTimer) {
-    send_announcement().then(() => {
-        context.log('Weekly announcement was posted!')
-    }).catch(e => context.log(e));
+    if (myTimer.isPastDue) {
+        send_announcement().then(() => {
+            context.log('Weekly announcement was posted!')
+        }).catch(e => context.log(e));    
+    }
 };
 
 // taken liberally from https://stackoverflow.com/questions/47548081/send-scheduled-message
 // send notification of weekly games night
-async function send_announcement() {
+function send_announcement() {
     const Discord = require('discord.js');
     const client = new Discord.Client();
-    return await client.login(process.env.CLIENT_ID).then(() => {
+    return client.login(process.env.CLIENT_ID).then(() => {
         var guild = client.guilds.get(process.env.GUILD_ID);
         if (guild && guild.channels.get(process.env.CHANNEL_ID)) {
             guild.channels.get(process.env.CHANNEL_ID).send(
