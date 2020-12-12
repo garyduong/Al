8379e9857eb6b72681e7b367d3ea535ede1c157b
Gary Duong <gx.duong@gmail.com>
2020-12-11 18:06:09 -0800

    Back to vanilla. Switch to Heroku

---
+++ index.js
@@ -0,0 +1,18 @@
+send_announcement().then(() => {
+    context.log('Weekly announcement was posted!')
+}).catch(e => context.log(e));    
+
+// taken liberally from https://stackoverflow.com/questions/47548081/send-scheduled-message
+// send notification of weekly games night
+function send_announcement() {
+    const Discord = require('discord.js');
+    const client = new Discord.Client();
+    return client.login(process.env.CLIENT_ID).then(() => {
+        var guild = client.guilds.get(process.env.GUILD_ID);
+        if (guild && guild.channels.get(process.env.CHANNEL_ID)) {
+            guild.channels.get(process.env.CHANNEL_ID).send(
+                '@everyone There will be a games night Friday at 8. Please let us know if you can join!'
+            ).then(() => client.destroy());
+        }
+    });
+}
