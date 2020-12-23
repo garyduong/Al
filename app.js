d0812164315973f791ced77c18d75869c8aa2346
Gary Duong <gx.duong@gmail.com>
2020-12-22 18:34:00 -0800

    Bot token for auth

--- app.js
+++ app.js
@@ -1,18 +1,7 @@
-send_announcement().then(() => {
-    console.log('Weekly announcement was posted!')
-}).catch(e => console.log(e));    
+var graph = require('./graph');
+var send = require('./notify');
 
-// taken liberally from https://stackoverflow.com/questions/47548081/send-scheduled-message
 // send notification of weekly games night
-function send_announcement() {
-    const Discord = require('discord.js');
-    const client = new Discord.Client();
-    return client.login(process.env.CLIENT_TOKEN).then(() => {
-        var guild = client.guilds.get(process.env.GUILD_ID);
-        if (guild && guild.channels.get(process.env.CHANNEL_ID)) {
-            guild.channels.get(process.env.CHANNEL_ID).send(
-                '@everyone There will be a games night Friday at 8. Please let us know if you can join!'
-            ).then(() => client.destroy());
-        }
-    });
-}
+send.weekly(process.env.BOT_TOKEN).then(() => {
+    console.log('Weekly announcement was posted!');
+}).catch(e => console.log(e));    
---
+++ notify.js
@@ -0,0 +1,13 @@
+// taken liberally from https://stackoverflow.com/questions/47548081/send-scheduled-message
+var weekly = function (bot_token) {
+    const Discord = require('discord.js');
+    const client = new Discord.Client();
+    return client.login(bot_token).then(() => {
+        var guild = client.guilds.get(process.env.GUILD_ID);
+        if (guild && guild.channels.get(process.env.CHANNEL_ID)) {
+            guild.channels.get(process.env.CHANNEL_ID).send(
+                '@everyone There will be a games night Friday at 8. Please let us know if you can join!'
+            ).then(() => client.destroy());
+        }
+    });
+}
