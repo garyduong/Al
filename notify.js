ac7e391157c43a4c06aed2c0a653c5993974e913
Gary Duong <gx.duong@gmail.com>
2020-12-22 19:40:29 -0800

    Updates to properties in API

--- notify.js
+++ notify.js
@@ -3,9 +3,9 @@ var weekly = function (bot_token) {
     const Discord = require('discord.js');
     const client = new Discord.Client();
     return client.login(bot_token).then(() => {
-        var guild = client.guilds.get(process.env.GUILD_ID);
-        if (guild && guild.channels.get(process.env.CHANNEL_ID)) {
-            guild.channels.get(process.env.CHANNEL_ID).send(
+        var guild = client.guilds.cache.get(process.env.GUILD_ID);
+        if (guild && guild.channels.cache.get(process.env.CHANNEL_ID)) {
+            guild.channels.cache.get(process.env.CHANNEL_ID).send(
                 '@everyone There will be a games night Friday at 8. Please let us know if you can join!'
             ).then(() => client.destroy());
         }
